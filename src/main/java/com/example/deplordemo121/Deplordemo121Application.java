a957f987e193ebdf374e6c01709a049bf29e0cd3
Thivagar <thivagarr1520@gmail.com>
2025-09-04 10:57:44 +0530

    first commit

---
+++ src/main/java/com/example/deplordemo121/Deplordemo121Application.java
@@ -0,0 +1,13 @@
+package com.example.deplordemo121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deplordemo121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deplordemo121Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deplordemo121/Deplordemo121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deplordemo121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deplordemo121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
